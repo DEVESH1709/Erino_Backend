fe1213ca6a070844822fedc945b91739837b9686
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-09-13 16:22:43 +0530

    Fix route registration for authentication and leads in the main server file

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-
+app.use('/api/auth',authRoutes);
+app.use ('/api/leads',leadsRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
