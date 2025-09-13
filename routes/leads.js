87ce6849f68126827c5f36ac3568af1ae7cf7f6a
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-09-13 16:19:40 +0530

    Implement authentication and leads routes with necessary CRUD operations

---
+++ routes/leads.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const leadController = require('../controllers/leadController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/',authMiddleware,leadController.createLead);
+router.get('/',authMiddleware,leadController.getLeads);
+router.get('/:id',authMiddleware,leadController.updateLead);
+router.put('/:id',authMiddleware,leadController.updateLead);
+router.delete('/:id',authMiddleware,leadController.deleteLead);
+
+
+module.exports = router;
